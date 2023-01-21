c9506b61d9e56f951a1fb6177db1e9147b662cd3
developer-mahin <mdmahin1310@gmail.com>
2023-01-21 21:19:34 +0600

    added some style in the footer section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
         <>
             <div className='container mx-auto grid lg:grid-cols-3 grid-cols-1 gap-20 justify-items-center'>
                 <div>
-                    <h2>About Company</h2>
+                    <h2 className='text-3xl font-bold text-gray-700'>About Company</h2>
                     <p>it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
                 </div>
                 <div>
-                    <h2>Quick Links</h2>
+                    <h2 className='text-3xl font-bold text-gray-700'>Quick Links</h2>
                     <div>
                         <Link className='block lg:my-0 my-2' to="/">Home</Link>
                         <Link className='block lg:my-0 my-2' to="products">Products</Link>
@@ -19,11 +19,10 @@ const Footer = () => {
                     </div>
                 </div>
                 <div>
-                    <h2>Contact Us</h2>
+                    <h2 className='text-3xl font-bold text-gray-700'>Contact Us</h2>
                     <p><a href="mailto:info@company.com">info@company.com</a></p>
                     <p><a href='tel:+91 079 00000000'>+91 079 00000000</a></p>
                     <div>
-
                     </div>
                 </div>
             </div>
